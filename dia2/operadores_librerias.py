e3c3fedbda346334966493f23efbf7b91c5791c8
Yanina Belmar <y.belmar01@gmail.com>
2024-03-13 13:15:01 -0300

    conociendo librerias y operadores

---
+++ dia2/operadores_librerias.py
@@ -0,0 +1,10 @@
+#import math  libreria
+from math import pow,sqrt,ceil
+print(2 ** 4 )# Exponenciación/Potencia. Ej: 2 elevado a 4 es 16
+print(5 // 2) # División Entera. Ej: 5 dividido 2 es 2.
+print(5 / 2 )# División con decimales. Ej: 5 dividido 2 es 2.5
+print(5 % 2) # Módulo. Ej: 5 Módulo 2 es 1. 1 es el resto de 5 // 2
+
+#print(math.pow(4,2)) se usa cuando se importa la libreria
+#print(math.sqrt(100)) se anteponde el print junto al math
+print(pow(4,2)) #aqui usando el from import importando solo los que yo quiero ocupar sin el math
