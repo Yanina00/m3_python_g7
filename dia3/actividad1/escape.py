b71b87730ad8b241f422ceb12cc3c766f431ae8c
Yanina Belmar <y.belmar01@gmail.com>
2024-03-14 12:06:15 -0300

    calculando la velocidad de escape

--- dia3/actividad1/escape.py
+++ dia3/actividad1/escape.py
@@ -2,7 +2,9 @@
 import  math
 
 #calculando la velocidad de escape 
-radio=float(input("Ingrese el radio del planeta en metros: "))
-gravedad=float(input("Ingrese la constante gravitacional: ""m/s^2"))
-velocidad_escape=math.sqrt(2 * radio * gravedad)
-print=(f"la velocidad del escape es:{velocidad_escape} m/s" )
+radio1=float(input("Ingrese el radio del planeta en km: "))
+gravedad=float(input("Ingrese la constante gravitacional en m/s^2: "))
+radio2=float(radio1 * 1000)
+velocidad_escape=math.sqrt(2 * radio2 * gravedad)
+
+print(f"la velocidad del escape es:{round(velocidad_escape,1)} m/s" )
--- dia3/actividad2/emprendedor1.py
+++ dia3/actividad2/emprendedor1.py
@@ -1,6 +1,7 @@
+#calculando utilidades del proyecto comida de mascotas
 
 precio_suscripcion=int(input("ingrese precio de suscripcion: "))
 numero_usuario=int(input("ingrese cantidad de usuario: "))
 gastos_totales:float(input("ingrese gastos totales: "))
-utilidades= calcular_utilidades (precio_suscripcion, numero_usuario, gastos_totales)
+utilidades=float(precio_suscripcion * numero_usuario - gastos_totales)
 print(f"las utilidades del proyecto son :{utilidades}")
