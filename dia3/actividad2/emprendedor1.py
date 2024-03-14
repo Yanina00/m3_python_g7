1a76355cafb2a8dc39f7c002359ddb65c6671131
Yanina Belmar <y.belmar01@gmail.com>
2024-03-14 12:47:24 -0300

    calculando utilidades

--- dia3/actividad2/emprendedor1.py
+++ dia3/actividad2/emprendedor1.py
@@ -1,7 +1,7 @@
 #calculando utilidades del proyecto comida de mascotas
 
-precio_suscripcion=int(input("ingrese precio de suscripcion: "))
+precio_suscripcion=float(input("ingrese precio de suscripcion: "))
 numero_usuario=int(input("ingrese cantidad de usuario: "))
-gastos_totales:float(input("ingrese gastos totales: "))
-utilidades=float(precio_suscripcion * numero_usuario - gastos_totales)
+gastos_totales=float(input("ingrese gastos totales: "))
+utilidades=(precio_suscripcion * numero_usuario - gastos_totales)
 print(f"las utilidades del proyecto son :{utilidades}")
---
+++ dia3/actividad2/emprendedor2.py
@@ -0,0 +1,10 @@
+#el emprendedor considera 2 tipos de usuarios diferenciados,
+#los usuarios normales y los usuarios premium a los cuales se les cobrará una
+#suscripción un 50% mayor.
+
+precio_suscripcion=float(input("ingrese precio de suscripcion: "))
+numero_usuario=int(input("ingrese cantidad de usuario: "))
+numero_usuario_premium=int(input("ingrese la cantidad ed ususario premium: "))
+gastos_totales=float(input("ingrese gastos totales: "))
+utilidades=((precio_suscripcion * numero_usuario) +(precio_suscripcion * 1.5 * precio_suscripcion) - gastos_totales)
+print(f"las utilidades del proyecto son :{utilidades}")
